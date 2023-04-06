0d97c5831ac85550c661ca857e78339daf876bcf
Александр Ячменев <yachmenyov.alex@yandex.ru>
2023-04-06 19:56:37 +0700

    Lesson 2 permission.

--- cats/views.py
+++ cats/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets, permissions
 
+from .permissions import OwnerOrReadOnly, ReadOnly
+
 from .models import Achievement, Cat, User
 
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
@@ -9,10 +11,18 @@ class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     # Устанавливаем разрешение
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (OwnerOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        # Если в GET-запросе требуется получить информацию об объекте
+        if self.action == 'retrieve':
+            # Вернем обновленный перечень используемых пермишенов
+            return (ReadOnly(),)
+        # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
+        return super().get_permissions()
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
