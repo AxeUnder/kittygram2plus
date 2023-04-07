3c03852192e0e2c1b70e9a7c65ea62ae54014ac6
Александр Ячменев <yachmenyov.alex@yandex.ru>
2023-04-08 02:57:35 +0700

    Lesson 5.

--- cats/views.py
+++ cats/views.py
@@ -22,13 +22,13 @@ class CatViewSet(viewsets.ModelViewSet):
     # Если кастомный тротлинг-класс вернёт True - запросы будут обработаны
     # Если он вернёт False - все запросы будут отклонены
     throttle_classes = (WorkingHoursRateThrottle,)
-    # Добавим в кортеж ещё один бэкенд
-    filter_backends = (filters.SearchFilter,)
-    # Временно отключим пагинацию на уровне вьюсета,
-    # так будет удобнее настраивать фильтрацию
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
     pagination_class = None
-    # Определим, что значение параметра search должно быть началом искомой строки
-    search_fields = ('^name',)
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('name',)
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
